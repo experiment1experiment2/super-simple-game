e86b2f3acafb9fea10731dba469cc2c259ee90d1
experiment1experiment2 <136436794+experiment1experiment2@users.noreply.github.com>
2023-08-14 14:13:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,93 @@
-let timerId;
-
-document.getElementById("start-button").addEventListener("click", () => {
-  document.getElementById("start-page").style.display = "none";
-  document.getElementById("game-page").style.display = "block";
-  document.getElementById("reset-button").style.display = "none";
-  document.getElementById("back-button").style.display = "inline-block";
-  startGame();
-});
-
-document.getElementById("reset-button").addEventListener("click", () => {
-  document.getElementById("reset-button").style.display = "none";
-  document.getElementById("result").innerHTML = "";
-  startGame();
-});
-
-document.getElementById("back-button").addEventListener("click", () => {
-  document.getElementById("game-page").style.display = "none";
-  document.getElementById("start-page").style.display = "block";
-  document.getElementById("back-button").style.display = "none";
-});
-
-function startGame() {
+document.addEventListener("DOMContentLoaded", () => {
+  const startPage = document.getElementById("start-page");
+  const gamePage = document.getElementById("game-page");
+  const resetButton = document.getElementById("reset-button");
+  const backButton = document.getElementById("back-button");
+  const timerDisplay = document.getElementById("timer");
+  const numbersDisplay = document.getElementById("numbers");
+  const buttonsContainer = document.getElementById("buttons");
+  const resultDisplay = document.getElementById("result");
+  
+  let timerId;
   let timeLeft = 60;
-  document.getElementById("timer").innerHTML = timeLeft;
-  document.getElementById("numbers").innerHTML = "";
-  document.getElementById("buttons").innerHTML = "";
+  let bots = [];
 
-  let bot1 = Math.floor(Math.random() * 101);
-  let bot2 = Math.floor(Math.random() * 101);
-  let bot3 = Math.floor(Math.random() * 101);
-  let bots = [bot1, bot2, bot3];
+  document.getElementById("start-button").addEventListener("click", () => {
+    startPage.style.display = "none";
+    gamePage.style.display = "block";
+    resetButton.style.display = "none";
+    backButton.style.display = "inline-block";
+    startGame();
+  });
 
-  timerId = setInterval(() => {
-    if (timeLeft > 0) {
-      timeLeft--;
-      document.getElementById("timer").innerHTML = timeLeft;
-    } else {
-      clearInterval(timerId);
-      calculateResult(null, bots);
+  resetButton.addEventListener("click", () => {
+    resetButton.style.display = "none";
+    resultDisplay.innerHTML = "";
+    startGame();
+  });
+
+  backButton.addEventListener("click", () => {
+    gamePage.style.display = "none";
+    startPage.style.display = "block";
+    backButton.style.display = "none";
+  });
+
+  function startGame() {
+    timeLeft = 60;
+    timerDisplay.innerHTML = timeLeft;
+    numbersDisplay.innerHTML = "";
+    buttonsContainer.innerHTML = "";
+    bots = Array.from({ length: 3 }, () => Math.floor(Math.random() * 101));
+
+    timerId = setInterval(() => {
+      if (timeLeft > 0) {
+        timeLeft--;
+        timerDisplay.innerHTML = timeLeft;
+      } else {
+        clearInterval(timerId);
+        calculateResult(null, bots);
+      }
+    }, 1000);
+
+    for (let i = 0; i <= 100; i++) {
+      const button = document.createElement("button");
+      button.innerHTML = i;
+      button.addEventListener("click", () => {
+        selectNumber(i, bots);
+      });
+      buttonsContainer.appendChild(button);
     }
-  }, 1000);
-
-  for (let i = 0; i <= 100; i++) {
-    let button = document.createElement("button");
-    button.innerHTML = i;
-    button.addEventListener("click", () => {
-      selectNumber(i, bots);
-    });
-    document.getElementById("buttons").appendChild(button);
   }
-}
 
-function selectNumber(playerNumber, bots) {
-  clearInterval(timerId);
-  let numbers = document.getElementById("numbers");
-  numbers.innerHTML = `You selected ${playerNumber}. Bots selected ${bots[0]}, ${bots[1]}, ${bots[2]}.`;
-  calculateResult(playerNumber, bots);
-}
+  function selectNumber(playerNumber, bots) {
+    clearInterval(timerId);
+    numbersDisplay.textContent = `You selected ${playerNumber}. Bots selected ${bots.join(", ")}.`;
+    calculateResult(playerNumber, bots);
+  }
 
-function calculateResult(playerNumber, bots) {
-  let total = 0;
-  for (let i = 0; i < bots.length; i++) {
-    total += bots[i];
-  }
-  if (playerNumber !== null) {
-    total += playerNumber;
-  }
-  let average = total / 4;
-  let calculatedNumber = average * 0.8;
-  let result = document.getElementById("result");
-  let message;
-  if (playerNumber === null) {
-    message = `Time's up! Bots selected ${bots[0]}, ${bots[1]}, ${bots[2]}.`;
-  } else {
-    let closestNumber = bots[0];
-    for (let i = 0; i < bots.length; i++) {
-      if (Math.abs(bots[i] - calculatedNumber) < Math.abs(closestNumber - calculatedNumber)) {
-        closestNumber = bots[i];
+  function calculateResult(playerNumber, bots) {
+    const total = bots.reduce((acc, bot) => acc + bot, 0);
+    if (playerNumber !== null) {
+      total += playerNumber;
+    }
+    const average = total / 4;
+    const calculatedNumber = average * 0.8;
+
+    if (playerNumber === null) {
+      resultDisplay.textContent = `Time's up! Bots selected ${bots.join(", ")}.`;
+    } else {
+      const closestNumber = bots.reduce((closest, bot) => (
+        Math.abs(bot - calculatedNumber) < Math.abs(closest - calculatedNumber) ? bot : closest
+      ));
+
+      if (Math.abs(playerNumber - calculatedNumber) < Math.abs(closestNumber - calculatedNumber)) {
+        resultDisplay.textContent = `You won! Your number ${playerNumber} was closest to ${calculatedNumber}.`;
+      } else {
+        resultDisplay.textContent = `You lost! ${closestNumber} was closest to ${calculatedNumber}.`;
       }
     }
-    if (Math.abs(playerNumber - calculatedNumber) < Math.abs(closestNumber - calculatedNumber)) {
-      message = `You won! Your number ${playerNumber} was closest to ${calculatedNumber}.`;
-    } else {
-      message = `You lost! ${closestNumber} was closest to ${calculatedNumber}.`;
-    }
+    resetButton.style.display = "block";
   }
-  result.innerHTML = message;
-  document.getElementById("reset-button").style.display = "block";
-}
 
-startGame();
+  startGame();
+});
